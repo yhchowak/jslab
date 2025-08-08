3035e114aa0bb688073417693962050945b49c4e
yhchowak <yhchowak@connect.ust.hk>
2025-08-08 12:08:23 +0800

    Added employee management system files

---
+++ employee_details.js
@@ -0,0 +1,37 @@
+const employees = [
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    //... More employee records can be added here
+  ];
+
+// display all employees details in totalEmployees --> use Map
+function displayEmployees(){
+    const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+        document.getElementById('employeesDetails').innerHTML = totalEmployees;
+  }
+
+// calculate employees' total salaries --> use Reduce
+function calculateTotalSalaries() {
+    const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
+    alert(`Total Salaries: $${totalSalaries}`);
+  }
+
+// display employees details based on department such as the HR department
+function displayHREmployees() {
+     const hrEmployees = employees.filter(employee => employee.department === 'HR');
+      const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+      document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
+}
+
+//display employees' details based on ID --> use Find
+function findEmployeeById(employeeId) {
+    const foundEmployee = employees.find(employee => employee.id === employeeId);
+    if (foundEmployee) {
+    document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    }
+    else{
+      document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+     }
+ }
+
